eea8f305980fa3ed4f3a8b3b1c2adbefee6f7bb3
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-03-27 22:17:42 +0530

    login page created and intent passing

--- app/src/main/java/com/example/healthapp/Welcome.java
+++ app/src/main/java/com/example/healthapp/Welcome.java
@@ -1,6 +1,9 @@
 package com.example.healthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Welcome extends AppCompatActivity {
+    private Button loginbutton;
+    private Button signupbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginbutton=findViewById(R.id.login_btn);
+        signupbutton=findViewById(R.id.signup_btn);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iswelcome=new Intent(Welcome.this, login.class);
+                startActivity(iswelcome);
+
+
+            }
         });
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent issignup=new Intent();
+            }
+        });
+
+
+
+
+
     }
 }
