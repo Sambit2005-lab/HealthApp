8959f31a7105e74bc080d63bb7e1e8fff186eeca
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-03-29 15:02:25 +0530

    profile fragment backend created

--- app/src/main/java/com/example/healthapp/profileFragment.java
+++ app/src/main/java/com/example/healthapp/profileFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class profileFragment extends Fragment {
@@ -27,6 +28,57 @@ public class profileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         layout= inflater.inflate(R.layout.fragment_profile, container, false);
+        Button button1 = layout.findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
+        Button button2 = layout.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
+        Button button3 = layout.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
+        Button button4 = layout.findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
+        Button button5 = layout.findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
+        Button backButton = layout.findViewById(R.id.button_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+            }
+        });
+
         return layout;
     }
 }
+
+
+
