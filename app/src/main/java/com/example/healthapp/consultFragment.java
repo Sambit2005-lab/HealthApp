3bcef464e4c48985ef0f50fd2386bf99a17dd186
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-03-29 19:30:39 +0530

    new lottie created

--- app/src/main/java/com/example/healthapp/consultFragment.java
+++ app/src/main/java/com/example/healthapp/consultFragment.java
@@ -8,9 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class consultFragment extends Fragment {
+
+    TextView detailText;
+
     View layout;
     Activity activity;
 
@@ -28,6 +33,8 @@ public class consultFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         layout= inflater.inflate(R.layout.fragment_consult, container, false);
+
+
         return layout;
     }
 }
