e524daed195b39ecd802a5737e2654115ee98a0d
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-03-29 19:44:24 +0530

    new lottie created

--- app/src/main/java/com/example/healthapp/consultFragment.java
+++ app/src/main/java/com/example/healthapp/consultFragment.java
@@ -1,9 +1,12 @@
 package com.example.healthapp;
 
+import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.transition.AutoTransition;
+import androidx.transition.TransitionManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +16,12 @@ import android.widget.TextView;
 
 
 public class consultFragment extends Fragment {
+    TextView detailsText;
+    LinearLayout layout;
 
-    TextView detailText;
 
-    View layout;
-    Activity activity;
+
+
 
 
 
@@ -29,12 +33,23 @@ public class consultFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        layout= inflater.inflate(R.layout.fragment_consult, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_consult, container, false);
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return layout;
+        detailsText = view.findViewById(R.id.doc02_details);
+        layout = view.findViewById(R.id.layout);
+        layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+    }
+
+    public void expand(View view) {
+        int v = (detailsText.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout, new AutoTransition());
+        detailsText.setVisibility(v);
     }
 }
